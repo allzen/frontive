db737b3331ca924ad99a30487ec4186c6aa8003b
Sha <shamandalie@op.pl>
2018-12-30 01:12:28 +0100

    Animated navbar, styled basket

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ let mainNav = document.getElementById('nav-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 
-navBarToggle.addEventListener('click', function () {
+navBarToggle.addEventListener('click', () => {
     
     mainNav.classList.toggle('active');
 
 });
+
+let navbarBg = document.getElementById("nav");
+
+window.onscroll = () => {
+  "use strict";
+  if (document.body.scrollTop >= 300 || document.documentElement.scrollTop >= 300) {
+    navbarBg.classList.add("scroll");
+  } else {
+    navbarBg.classList.remove("scroll");
+  }
+};
