f2507682cca57cb959ea5566147f589d0f835d3b
Sha <shamandalie@op.pl>
2019-01-01 17:27:51 +0100

    Modal styled, added all heroes objects

--- script.js
+++ script.js
@@ -20,3 +20,53 @@ window.onscroll = () => {
     navbarBg.classList.remove("scroll");
   }
 };
+
+
+const superman = {
+    name: 'Superman',
+    description: 'Hero description lorem....',
+    image: 'superman.jpg',
+    price: '9999',
+    isAvailable: true
+ }
+
+ 
+const hulk = {
+    name: 'hulk',
+    description: 'Hero description lorem....',
+    image: 'hulk.jpg',
+    price: '9999',
+    isAvailable: true
+ }
+
+ const thor = {
+    name: 'thor',
+    description: 'Hero description lorem....',
+    image: 'thor.jpg',
+    price: '9999',
+    isAvailable: true
+ }
+ 
+ const ironman = {
+    name: 'ironman',
+    description: 'Hero description lorem....',
+    image: 'ironman.jpg',
+    price: '9999',
+    isAvailable: true
+ }
+
+ const potter = {
+    name: 'potter',
+    description: 'Hero description lorem....',
+    image: 'potter.jpg',
+    price: '9999',
+    isAvailable: true
+ }
+
+ const batman = {
+    name: 'batman',
+    description: 'Hero description lorem....',
+    image: 'batman.jpg',
+    price: '9999',
+    isAvailable: true
+ }
